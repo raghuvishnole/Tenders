c354b8269949b8660c14357d3c77e6c8a58de642
raghunandan <raghuvishnole143@gmail.com>
2025-04-22 17:11:12 +0530

    pass

--- API/models/connection.js
+++ API/models/connection.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 const url =
-  "mongodb+srv://raghuvishnole143:<db_password>@cluster0.yb4mwr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://raghuvishnole143:admin@cluster0.yb4mwr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(url);
 console.log("Successfully connected to mongodb database...");
