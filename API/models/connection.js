b64fceb39dfc64a88cb0e45c1d1b62fec56aba90
raghunandan <raghuvishnole143@gmail.com>
2025-04-22 17:05:50 +0530

    connect to mongo db

--- API/models/connection.js
+++ API/models/connection.js
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose';
-const url="mongodb://localhost:27017/Tenders";
+import mongoose from "mongoose";
+const url =
+  "mongodb+srv://raghuvishnole143:<db_password>@cluster0.yb4mwr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(url);
 console.log("Successfully connected to mongodb database...");
